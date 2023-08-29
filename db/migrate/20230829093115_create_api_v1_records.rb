2cf39a192caeba5b974b61bdc6329bd27bb36718
Javier Puebla <jpuebla@netquest.com>
2023-08-29 11:35:11 +0200

    migrate db and add schema

--- db/migrate/20230829093115_create_api_v1_records.rb
+++ db/migrate/20230829093115_create_api_v1_records.rb
@@ -3,6 +3,9 @@ class CreateApiV1Records < ActiveRecord::Migration[7.0]
     create_table :api_v1_records do |t|
 
       t.timestamps
+
+      t.string :title
+      t.text :img
     end
   end
 end
