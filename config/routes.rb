dfeac762e856e17151d47a3cf69a06996936906f
Javier Puebla <jpuebla@netquest.com>
2023-10-06 14:01:17 +0200

    update users routes for signin and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      post '/signin/', to: 'users#signin'
+      post '/login/', to: 'users#login'
       resources :records
     end
   end
