62f86f97f9880d3d7a34352fef2a2f4b567e00bf
Javier Puebla <jpuebla@netquest.com>
2023-11-03 13:24:14 +0100

    add jwt method to User model

--- app/models/api/v1/user.rb
+++ app/models/api/v1/user.rb
@@ -1,2 +1,15 @@
 class Api::V1::User < ApplicationRecord
+    validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+
+    def jwt
+        to_encode = {
+            "user_id": self.id,
+            "exp": Time.now.utc.to_i + Rails.application.credentials.access_token_expire_minutes * 60,
+        }
+        access_token = JWT.encode(
+            to_encode, Rails.application.credentials.secret_key, Rails.application.credentials.algorithm
+        )
+
+        access_token
+    end
 end
